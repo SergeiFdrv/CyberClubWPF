6b07a3c9179328b109ee3d11d7a0ed00bf064347
sergfedoro@mail.ru <sergfedoro@mail.ru>
2020-11-06 13:19:49 +0300

    Add functionality to AdminPage

--- CyberClub/AdminWindow.xaml.cs
+++ CyberClub/AdminWindow.xaml.cs
@@ -24,32 +24,47 @@ namespace CyberClub
         {
             InitializeComponent();
         }
+        protected static App Global => Application.Current as App;
 
         private GamesPage GamesPage { get; } = new GamesPage();
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            GamesButton.IsChecked = TableTabButton.IsChecked = true;
+        }
 
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             Owner.Show();
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
         {
+
+        }
+
+        private void GamesButton_Checked(object sender, RoutedEventArgs e)
+        {
+            AccountsButton.IsChecked = MessagesButton.IsChecked = GamesButton.IsEnabled =
+                !(AccountsButton.IsEnabled = MessagesButton.IsEnabled = true);
+            DataGridTable.ItemsSource = Global.DB.Games.ToList();
             PageFrame.Navigate(GamesPage);
         }
 
-        private void GamesButton_Click(object sender, RoutedEventArgs e)
+        private void AccountsButton_Checked(object sender, RoutedEventArgs e)
         {
-            PageFrame.Navigate(GamesPage);
+            GamesButton.IsChecked = MessagesButton.IsChecked = AccountsButton.IsEnabled =
+                !(GamesButton.IsEnabled = MessagesButton.IsEnabled = true);
+            DataGridTable.ItemsSource = Global.DB.Users.ToList();
+            //PageFrame.Navigate(AccountsPage);
         }
 
-        private void AccountsButton_Click(object sender, RoutedEventArgs e)
+        private void MessagesButton_Checked(object sender, RoutedEventArgs e)
         {
-
-        }
-
-        private void MessagesButton_Click(object sender, RoutedEventArgs e)
-        {
-
+            GamesButton.IsChecked = AccountsButton.IsChecked = MessagesButton.IsEnabled =
+                !(GamesButton.IsEnabled = AccountsButton.IsEnabled = true);
+            DataGridTable.ItemsSource = Global.DB.Feedbacks.ToList();
+            //PageFrame.Navigate(MessagesPage);
         }
 
         private void LogOutButton_Click(object sender, RoutedEventArgs e)
@@ -59,19 +74,28 @@ namespace CyberClub
 
         private void TableTabButton_Checked(object sender, RoutedEventArgs e)
         {
-            AddTabButton.IsChecked = EditTabButton.IsChecked = false;
+            AddTabButton.IsChecked = EditTabButton.IsChecked = TableTabButton.IsEnabled =
+                !(AddTabButton.IsEnabled = EditTabButton.IsEnabled = true);
+            DataGridTable.Visibility = Visibility.Visible;
+            PageFrame.Visibility = Visibility.Collapsed;
         }
 
         private void AddTabButton_Checked(object sender, RoutedEventArgs e)
         {
-            TableTabButton.IsChecked = EditTabButton.IsChecked = false;
-            (PageFrame.Content as GamesPage).IsInEditMode = false;
+            TableTabButton.IsChecked = EditTabButton.IsChecked = AddTabButton.IsEnabled =
+                !(TableTabButton.IsEnabled = EditTabButton.IsEnabled = true);
+            (PageFrame.Content as AdminEditorPage).IsInEditMode = false;
+            PageFrame.Visibility = Visibility.Visible;
+            DataGridTable.Visibility = Visibility.Collapsed;
         }
 
         private void EditTabButton_Checked(object sender, RoutedEventArgs e)
         {
-            TableTabButton.IsChecked = AddTabButton.IsChecked = false;
-            (PageFrame.Content as GamesPage).IsInEditMode = true;
+            TableTabButton.IsChecked = AddTabButton.IsChecked = EditTabButton.IsEnabled =
+                !(TableTabButton.IsEnabled = AddTabButton.IsEnabled = true);
+            (PageFrame.Content as AdminEditorPage).IsInEditMode = true;
+            PageFrame.Visibility = Visibility.Visible;
+            DataGridTable.Visibility = Visibility.Collapsed;
         }
     }
 }
