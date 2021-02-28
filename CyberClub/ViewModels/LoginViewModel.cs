ae9044028e6cbfffb1759f44882c243396123f13
sergfedoro@mail.ru <sergfedoro@mail.ru>
2021-02-28 19:11:04 +0300

    Add MVVM commands, add LoginWindow ViewModel with one command

---
+++ CyberClub/ViewModels/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberClub.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        private RelayCommand _FocusOnNext;
+        public RelayCommand FocusOnNext
+        {
+            get => _FocusOnNext ?? (_FocusOnNext = new RelayCommand(obj =>
+            {
+                //
+            }));
+        }
+    }
+}
---
+++ CyberClub/ViewModels/RelayCommand.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CyberClub.ViewModels
+{
+    /// <summary>
+    /// Code from https://metanit.com/sharp/wpf/22.3.php
+    /// </summary>
+    public class RelayCommand : ICommand
+    {
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _Execute = execute;
+            _CanExecute = canExecute;
+        }
+
+        private Action<object> _Execute;
+        private Func<object, bool> _CanExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _CanExecute == null || _CanExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _Execute(parameter);
+        }
+    }
+}
